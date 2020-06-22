0b4ce58d7bec31bdce4b4e0a2772c7d60d80fbf6
jesperberth <jesper.berth@arrow.com>
2020-06-22 15:06:49 +0200

    get group - user

--- ad-migrate.ps1
+++ ad-migrate.ps1
@@ -48,12 +48,20 @@ function ExportSourceToCSV($ou){
     New-Item -ItemType "directory" -Path $exportpath -Force
     $exportgrouppath = $exportpath + $exportgroupfile
     $exportuserpath = $exportpath + $exportuserfile
-    write-host -foregroundcolor green "`n#####################################################################`n"
+    write-host -foregroundcolor green "#####################################################################`n"
     write-host -foregroundcolor green "Export all Groups and users to CSV file from: " $ou "`n"
     write-host -foregroundcolor green "#####################################################################`n"
-    get-adgroup -filter * -SearchBase $ou -Properties * | Select-Object DistinguishedName, Description, Name, GroupCategory, GroupScope | Export-Csv -Path $exportgrouppath -Encoding UTF8
+    get-adgroup -filter * -SearchBase $ou -Properties * | Select-Object DistinguishedName, Description, Name, GroupCategory, GroupScope | Export-Csv -Path $exportgrouppath -Encoding UTF8 -NoTypeInformation
     # Get user details
-    get-aduser -filter * -SearchBase $ou -Properties * | Select-Object DisplayName, City, CN, Company, Country, countryCode, Department, Description, Division, EmailAddress, EmployeeID, EmployeeNumber, Fax, GivenName, HomeDirectory, HomedirRequired, HomeDrive, HomePage, HomePhone, Initials, Manager, MobilePhone, Name, Office, OfficePhone, Organization, OtherName, POBox, PostalCode, ProfilePath, SamAccountName, ScriptPath, sn, State, StreetAddress, Surname, Title, UserPrincipalName | Export-Csv -Path $exportuserpath -Encoding UTF8
+    get-aduser -filter * -SearchBase $ou -Properties * | Select-Object DisplayName, City, CN, Company, Country, countryCode, Department, Description, Division, EmailAddress, EmployeeID, EmployeeNumber, Fax, GivenName, HomeDirectory, HomedirRequired, HomeDrive, HomePage, HomePhone, Initials, Manager, MobilePhone, Name, Office, OfficePhone, Organization, OtherName, POBox, PostalCode, ProfilePath, SamAccountName, ScriptPath, sn, State, StreetAddress, Surname, Title, UserPrincipalName | Export-Csv -Path $exportuserpath -Encoding UTF8 -NoTypeInformation
+
+$groups = (Import-Csv -Path $exportgrouppath).Name
+    
+    foreach($group in $groups){
+        write-host -ForegroundColor Cyan $group
+        get-adgroupmember -Identity $group | Select-Object SamAccountName
+    }
+
 }
 
 do
