cc2a43cff44bab0cf737472307a908be9eca906b
jesperberth <jesper.berth@arrow.com>
2020-06-23 09:41:17 +0200

    Import groups

--- ad-migrate.ps1
+++ ad-migrate.ps1
@@ -1,7 +1,11 @@
 # Menu driven AD Migration Tool
-#
+# Exports Groups, Users and Mappings between Groups and Users
 # Author: Jesper Berth, Arrow ECS, jesper.berth@arrow.com - 22/06-2020
 # Version 0.0.1
+$path = "C:\ExportOU\"
+$groupfile = "group.csv"
+$groupmembersfile = "groupmembers.csv"
+$userfile = "users.csv"
 function Show-Menu
 {
     param (
@@ -11,7 +15,7 @@ function Show-Menu
     Write-Host "======== $Title ========`n"
     Write-Host "1: Set Source/Destination OU for Export and Import"
     Write-Host "2: Export Groups and Users to CSV"
-    Write-Host "3: "
+    Write-Host "3: Import Groups and Users"
     Write-Host "Q: Press 'Q' to quit."
     Write-Host "==============================="
 }
@@ -25,10 +29,6 @@ function SetOU{
 }
 
 function ExportSourceToCSV($ou){
-    $exportpath = "C:\ExportOU\"
-    $exportgroupfile = "group.csv"
-    $exportgroupmembersfile = "groupmembers.csv"
-    $exportuserfile = "users.csv"
     if($null -eq $ou){
         write-host -ForegroundColor red "You Need to set the Export OU, press any key to continue"
         $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
@@ -40,17 +40,17 @@ function ExportSourceToCSV($ou){
 		write-host -foregroundcolor yellow "Default Export path C:\ExportOU\`n"
 		$response = Read-Host -Prompt $exportpathmsg
 		if ($response -eq 'y') {
-            $exportpath = Read-Host -Prompt "Set Export path"
-            write-host -foregroundcolor yellow "New export path: " $exportpath
+            $path = Read-Host -Prompt "Set Export path"
+            write-host -foregroundcolor yellow "New export path: " $path
 		$response = "n"
  		}
 	} 	until ($response -eq 'n')
-    New-Item -ItemType "directory" -Path $exportpath -Force
-    $exportgrouppath = $exportpath + $exportgroupfile
-    $exportuserpath = $exportpath + $exportuserfile
-    $exportgroupmemberspath = $exportpath + $exportgroupmembersfile
+    New-Item -ItemType "directory" -Path $path -Force
+    $exportgrouppath = $path + $groupfile
+    $exportuserpath = $path + $userfile
+    $exportgroupmemberspath = $path + $groupmembersfile
     write-host -foregroundcolor green "#####################################################################`n"
-    write-host -foregroundcolor green "Export all Groups and users to CSV file from: " $ou "`n"
+    write-host -foregroundcolor green "# Export all Groups and users to CSV file from: " $ou "`n"
     write-host -foregroundcolor green "#####################################################################`n"
     get-adgroup -filter * -SearchBase $ou -Properties * | Select-Object DistinguishedName, Description, Name, GroupCategory, GroupScope | Export-Csv -Path $exportgrouppath -Encoding UTF8 -NoTypeInformation
     # Get user details
@@ -74,7 +74,38 @@ function ExportSourceToCSV($ou){
             Add-Content -Path $exportgroupmemberspath -Value $groupmemberline
         }
     }
+    write-host -foregroundcolor green "#####################################`n"
+    write-host -foregroundcolor green "# Export of Users and Groups Done ! #`n"
+    write-host -foregroundcolor green "#####################################`n"
+}
 
+function ImportGroupsUsers {
+
+    $exportpathmsg = "Do you want to change Default Import path y/n"
+	do {
+		write-host -foregroundcolor yellow "Default Import path C:\ExportOU\`n"
+		$response = Read-Host -Prompt $exportpathmsg
+		if ($response -eq 'y') {
+            $path = Read-Host -Prompt "Set Import path"
+            write-host -foregroundcolor yellow "New Import path: " $path
+		$response = "n"
+ 		}
+    } 	until ($response -eq 'n')
+    $importgroupsfile = $path + "group.csv"
+    $fileNames = Get-ChildItem -Path $path
+    foreach ($file in $fileNames) {
+        write-host "file: $file`n"
+    }
+
+    $importgroupou = "OU=Import_Groups," + $ou
+    write-host "Import following AD Groups into $importgroupou`n"
+
+    $importedgroups = Import-Csv -path $importgroupsfile
+
+    foreach ($group in $importedgroups) {
+        write-host $group.Name
+        New-adgroup -Path $importgroupou -Name $group.Name -GroupScope $group.GroupScope -GroupCategory $group.GroupCategory -Description $group.Description
+    }
 }
 
 do
@@ -91,7 +122,7 @@ do
              ExportSourceToCSV $ou
          } '3' {
              Clear-Host
-             InstallCode
+             ImportGroupsUsers
          } '4' {
              Clear-Host
              InstallVideo
